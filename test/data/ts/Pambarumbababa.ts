85ae3bc3c8b97160f597e0bf9ec7a8fa7c8d2e82
Maksym Kobieliev <maximaximums@gmail.com>
2017-12-04 14:43:50 +0200

    Minor fix

--- test/data/ts/Pambarumbababa.ts
+++ test/data/ts/Pambarumbababa.ts
@@ -1,8 +1,8 @@
 namespace Models {
-  export interface Pambarumbababa extends Chuka {
-    Type: JugaType;
-    HamCurrency: Currency;
+  export interface Pambarumbababa extends Sample1 {
+    Type: WhyType;
+    HamCurrency: Importance;
     RoundQuantityFirst: boolean;
-    RoundingType: RoundingType;
+    RoundingType: ObjectType;
   }
 }
--- test/data/vb/Pambarumbababa.vb
+++ test/data/vb/Pambarumbababa.vb
@@ -1,10 +1,10 @@
 Namespace Models
   Public Class Pambarumbababa
-    Inherits Chuka
+    Inherits Sample1
 
-    Public Overloads Property Type As JugaType = JugaType.Supplier
-    Public Property HamCurrency As Currency
+    Public Overloads Property Type As WhyType = WhyType.Because
+    Public Property HamCurrency As Importance
     Public Property RoundQuantityFirst As Boolean
-    Public Property RoundingType As RoundingType
+    Public Property RoundingType As ObjectType
   End Class
 End Namespace
